cc905126e20ce328a45e099285bc704f9cd60e59
CMCarlaMecatti <canmecatti@gmail.com>
2023-07-07 18:46:29 -0300

    Actualizacion de Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import validation from "../Validation/Validation"
+import style from './Form.module.css'
 
 const Form= ({login}) => {
  const  [errors, setErrors] = useState({});
@@ -29,17 +30,23 @@ const handleSubmit = (event) => {
 }
 
     return(
-    <form onSubmit={handleSubmit} >
-        <label htmlFor="email">Email:</label>
-        <input type="email" name="email" value={userData.email} onChange={handleChange} />
-        {errors.email && <p>{errors.email}</p> }
-        <hr />
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password"  value={userData.password} onChange={handleChange} />
-        {errors.password && <p>{errors.password}</p> }
-        <hr />
-        <button>Submitt</button>
+    <div className={style.container}>
+        <div className={style.form_box}>
+        <form  onSubmit={handleSubmit} >
+        <div className={style.input_box}>
+        <label className={style.label} htmlFor="email">Email:</label>
+        <input type="email" name="email" value={userData.email} onChange={handleChange} className={style.input} />
+        {errors.email && <p className={style.p} >{errors.email}</p> }</div>
+        
+        <div className={style.input_box}>
+        <label className={style.label} htmlFor="password">Password:</label>
+        <input className={style.input} type="password" name="password"  value={userData.password} onChange={handleChange} />
+        {errors.password && <p className={style.p} >{errors.password}</p> }</div>
+        
+        <button className={style.button}>Submitt</button>
     </form>
+    </div>
+    </div>
  )
 }
 
